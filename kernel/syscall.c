3ecf79a0ae7cf9e5fb7a8c604d95ee0b05758693
callFlower <107831013+callFlower@users.noreply.github.com>
2023-03-05 15:03:27 +1100

    initial

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -136,8 +136,7 @@ static uint64 (*syscalls[])(void) = {
 static char* syscall_names[]={
        "fork", "exit","wait","pipe","read","kill","exec","fstat","chdir","dup"
        ,"getpid","sbrk","sleep","uptime","open","write","mknod","unlink","link"
-       ,"mkdir","close","trace","sysinfo"
-};
+       ,"mkdir","close","trace","sysinfo"};
 
 void
 syscall(void)
